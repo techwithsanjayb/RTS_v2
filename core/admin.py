9e9a8e22ce18bdaa1a8b9583b3d26f2f888f9690
techwithsanjayb <sanjayb.info@gmail.com>
2026-02-19 17:30:30 +0530

    DB Entry Created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(System)
+admin.site.register(Zone)
+admin.site.register(Division)
+admin.site.register(Department)
+admin.site.register(Coach_Type)
+admin.site.register(Category)
+admin.site.register(ResponsibleUnit)
+admin.site.register(SubCategory)
+admin.site.register(OfficerRole)
+admin.site.register(UserProfile)
+admin.site.register(EscalationLevel)
+admin.site.register(Issue)
+admin.site.register(TicketStatus)
+admin.site.register(Ticket)
+ 
